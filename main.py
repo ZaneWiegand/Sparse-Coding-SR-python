ca4bc45df9af5ec4b600f32c5ef724bc0b52db68
Cocytus-Leon <smilingbaboon@126.com>
2022-08-29 18:34:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 # %%
+import os
+import numpy as np
+from skimage import io
+from skimage.color import rgb2ycbcr,ycbcr2rgb,rgb2gray
 import pickle
 from sklearn.preprocessing import normalize
+from tqdm import tqdm
 # %%
+# choose parameters
 class args(object):
     lr_dir = 'data/set5_lr'
     sr_dir = 'result/set5_sr'
     hr_dir = 'data/set5_hr'
     
     # choose a dictionary for SR
-    upscale_factor = 2
-    lambda_factor = 0.1
-    dictionary_size = 1024
-    patch_size = 3
+    dic_upscale_factor = 2
+    dic_lambda = 0.1
+    dic_size = 1024
+    dic_patch_size = 3
     
-    val_flag = False
+    # Sparse SR factor
+    lambda_factor = 0.3
+    overlap = 1
+    upscale_factor = 2
+    max_iteration = 1000
+    color_space = 'ycbcr' # 'bw'
+    
+    # True for validation, False for prediction
+    val_flag = True
     
 para = args()
 # %%
-dict_name = str(para.dictionary_size) + '_US' + str(para.upscale_factor) + '_L' + str(para.lambda_factor) + '_PS' + str(para.patch_size)
+# load dictionary
+dict_name = str(para.dic_size) + '_US' + str(para.dic_upscale_factor) + '_L' + str(para.dic_lambda) + '_PS' + str(para.dic_patch_size)
 
 with open('dictionary/Dh_' + dict_name + '.pkl', 'rb') as f:
     Dh = pickle.load(f)
@@ -25,4 +40,21 @@ Dh = normalize(Dh)
 with open('dictionary/Dl_' + dict_name + '.pkl', 'rb') as f:
     Dl = pickle.load(f)
 Dl = normalize(Dl)
+# %%
+# super resolution img dir
+if not os.path.exists(para.sr_dir):
+    os.makedirs(para.sr_dir)
+# %%
+img_lr_file = os.listdir(para.lr_dir)
+# %%
+for i in tqdm(range(len(img_lr_file))):
+    img_name = img_lr_file[i]
+    img_lr = io.imread(f"{para.lr_dir}/{img_name}")
+    img_hr = io.imread(f"{para.hr_dir}/{img_name}")
+    
+    if para.color_space == 'ycbcr':
+        img_hr_y = rgb2ycbcr(img_hr)[:,:,0]
+        
+        # change color space
+        
 # %%
